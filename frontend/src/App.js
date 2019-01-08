3303e0d41b7bdc6fd62c7b3e3362e4d6535b6ad6
your <jurgenkela@gmail.com>
2019-01-08 18:27:04 -0500

    started the nav with SignUp and Sign In links and styled

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Nav from "./components/Nav";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <Nav />
       </div>
     );
   }
