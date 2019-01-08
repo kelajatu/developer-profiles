2ca0dd0945f0287d38011d6b1029821f95c60a23
JoCatherine Griggs <jocafrin@gmail.com>
2019-01-08 18:32:39 -0500

    displaying landing page, minimal styling done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
+import LandingPage from "./containers/landingPage";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <LandingPage />
       </div>
     );
   }
 }
 
 export default App;
+
