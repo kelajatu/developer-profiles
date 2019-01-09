05883be70a2b114e4a529c06f83372585ad31564
Alberto-Funk <laptopreneur@gmail.com>
2019-01-08 17:31:43 -0800

    Edit App to avoid merge conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from "react";
-import UserInitProfile from './containers/UserInitProfile';
-
-// flow
-// user registers -> Save to DB
-// user fills out info(profile seeker) -> Save to DB
-// user fills out billing -> save to DB -> List of users updated
-// send to profile(profile seeker?) - user can edit
-// user shows up on main pages, can view their own expanded profile
+import Nav from "./components/Nav";
+import LandingPage from "./containers/landingPage";
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        <UserInitProfile />
+        <Nav />
+        <LandingPage />
       </div>
     );
   }
