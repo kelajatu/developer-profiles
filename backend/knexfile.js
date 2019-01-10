218713b9c0db898545222ab9cb6cb1091a23ad18
mkerbleski <mkerbles@gmail.com>
2019-01-10 14:09:10 -0700

    deleted everything except production for knex

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -2,38 +2,16 @@
 require('dotenv').config()
 
 module.exports = {
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    },
-    useNullAsDefault: true,
-  },
-
-  amazon: {
-    client: 'mysql',
-    version: '5.6.41',
-    connection: {
-      host : process.env.RDS_HOSTNAME,
-      port     : process.env.RDS_PORT,
-      user : process.env.RDS_USERNAME,
-      password : process.env.RDS_PASSWORD,
-      database : process.env.RDS_DB_NAME
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    useNullAsDefault: true,
-  },
-
   production: {
-    client: 'pg',
+    client: 'postgresql',
     connection: process.env.DATABASE_URL,
     acquireConnectionTimeout: 10000,
     pool: {
       min: 2,
       max: 10
+    },
+    migrations: {
+      tablename: 'knex_migrations'
     }
   }
 };
