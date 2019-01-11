63870047612d687795bfe7acb0344409828c0f23
your <jurgenkela@gmail.com>
2019-01-11 10:00:53 -0500

    Get route testing

---
+++ backend/test/Index.js
@@ -0,0 +1,9 @@
+let chai = require("chai");
+let server = require("../index");
+let should = chai.should();
+
+describe("Get to Ingex.js", () => {
+  it("Can succesfully make a request", () => {
+    expect(response.status).to.equal(200);
+  });
+});
