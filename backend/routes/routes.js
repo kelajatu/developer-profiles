d3f3296fff64b9cb09bd50a1379be99d9a2d9ab3
mkerbleski <mkerbles@gmail.com>
2019-01-11 19:05:46 -0700

    formated apiRoutes for consistency

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const cors = require('cors')
-
 const router = express.Router();
 
 const userRoutes = require('./userRoutes')
@@ -8,11 +7,8 @@ const authRoutes = require('./authRoutes')
 const listRoutes = require('./listRoutes')
 const apiRoutes = require('./apiRoutes')
 
-router.use(express.json());
-
 router.use(express.json())
 router.use(cors())
-
 
 router.use("/users", userRoutes);
 router.use("/auth", authRoutes);
