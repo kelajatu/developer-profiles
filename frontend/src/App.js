c479492284b6da7c5a5c4e4b1d3040e390e18fa6
your <jurgenkela@gmail.com>
2019-01-14 18:01:20 -0500

    config current work for react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
-import { Route, Link } from "react-router-dom";
+import {
+  Route,
+  Link,
+  Redirect,
+  Switch,
+  BrowserRouter as Router
+} from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 import LandingPage from "./containers/landingPage";
 import BreadCrumbs from "./components/BreadCrumbs/BreadCrumbs";
@@ -32,37 +38,46 @@ const Billing = () => (
   </div>
 );
 
+const CustomLink = ({ children, to, exact }) => (
+  <Route
+    path={to}
+    exact={exact}
+    children={({ match }) => (
+      <div className={match ? "active" : ""}>
+        {match ? "> " : ""}
+        <Link to={to}>{children}</Link>
+      </div>
+    )}
+  />
+);
+
 class App extends Component {
   render() {
     return (
       <div>
         <ul>
           <li>
-            <Link exact to="/">
+            <CustomLink exact={true} to="/">
               Home
-            </Link>
+            </CustomLink>
           </li>
           <li>
-            <Link to="/usercard">User card</Link>
+            <CustomLink to="/usercard">User card</CustomLink>
           </li>
           <li>
-            <Link to="/userprofile">User profile</Link>
+            <CustomLink to="/userprofile">User profile</CustomLink>
           </li>
           <li>
-            <Link to="/billing">Billing</Link>
+            <CustomLink to="/billing">Billing</CustomLink>
           </li>
         </ul>
-
-        <Route path="/" exact component={Home} />
-        <Route path="/usercard" component={Usercard} />
-        <Route path="/userprofile" component={Userprofile} />
-        <Route path="/billing" component={Billing} />
-
-        {/* <BreadCrumbs />
-        <Nav />
-        <LandingPage />
-        <UserCard />
-        <UserInitProfile /> */}
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/usercard" component={Usercard} />
+          <Route path="/userprofile" component={Userprofile} />
+          <Route path="/billing" component={Billing} />
+          <Route render={() => <div> Page does not exist. </div>} />
+        </Switch>
       </div>
     );
   }
