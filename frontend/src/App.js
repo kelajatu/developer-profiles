f9658d827f927e29efff207d7d1a126f82e5f8fc
your <jurgenkela@gmail.com>
2019-01-14 16:39:42 -0500

    created router to clear up landing page mess

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,68 @@
 import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 import LandingPage from "./containers/landingPage";
 import BreadCrumbs from "./components/BreadCrumbs/BreadCrumbs";
 import UserCard from "./components/UserCard/UserCard";
 import UserInitProfile from "./containers/UserInitProfile";
 
+const Home = () => (
+  <div>
+    <BreadCrumbs />
+    <Nav />
+    <LandingPage />
+  </div>
+);
+
+const Usercard = () => (
+  <div>
+    <UserCard />
+  </div>
+);
+
+const Userprofile = () => (
+  <div>
+    <UserInitProfile />
+  </div>
+);
+
+const Billing = () => (
+  <div>
+    <h1>Billing Page</h1>
+  </div>
+);
+
 class App extends Component {
   render() {
     return (
       <div>
-        <BreadCrumbs />
+        <ul>
+          <li>
+            <Link exact to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/usercard">User card</Link>
+          </li>
+          <li>
+            <Link to="/userprofile">User profile</Link>
+          </li>
+          <li>
+            <Link to="/billing">Billing</Link>
+          </li>
+        </ul>
+
+        <Route path="/" exact component={Home} />
+        <Route path="/usercard" component={Usercard} />
+        <Route path="/userprofile" component={Userprofile} />
+        <Route path="/billing" component={Billing} />
+
+        {/* <BreadCrumbs />
         <Nav />
         <LandingPage />
         <UserCard />
-        <UserInitProfile />
+        <UserInitProfile /> */}
       </div>
     );
   }
