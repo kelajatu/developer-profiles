6a0cdc52efdd9b403427dc57dc431c2cc16cddd6
mkerbleski <mkerbles@gmail.com>
2019-01-16 16:03:53 -0700

    added basic skelton for filter component

--- frontend/src/pages/main-page/PublicFacingPage.js
+++ frontend/src/pages/main-page/PublicFacingPage.js
@@ -1,15 +1,19 @@
 import { PublicFacingPageDiv } from './PublicFacingPage.style'
 import UserCard from '../../components/user-card/UserCard'
+import FilterBox from '../../components/Filter/filter'
 import React, { Component } from 'react'
 
 class PublicFacingPage extends Component {
     render() { 
         return (
         <PublicFacingPageDiv>
-           <UserCard/>
-           <UserCard/>
-           <UserCard/>
-           <UserCard/>
+            <FilterBox />
+            <div className="cards">
+                <UserCard/>
+                <UserCard/>
+                <UserCard/>
+                <UserCard/>
+            </div>
         </PublicFacingPageDiv>  );
     }
 }
--- frontend/src/pages/main-page/PublicFacingPage.style.js
+++ frontend/src/pages/main-page/PublicFacingPage.style.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const PublicFacingPageDiv = styled.div`
     background: lightblue;
     display: flex;
-    flex-direction: column;
-    align-items: center;
+    flex-direction: row;
+    align-items: flex-start;
+    .cards{width: 50%}
 `
