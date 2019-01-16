8ccae14d86680fc51696a920b5480e0604d65566
mkerbleski <mkerbles@gmail.com>
2019-01-16 15:29:09 -0700

    deleted old auth files

--- frontend/src/components/Nav/Nav.js
+++ frontend/src/components/Nav/Nav.js
@@ -7,22 +7,20 @@ export default class Nav extends Component {
     console.log('NAV',this.props)
     return (
       <div className="nav">
-        
         <div onClick={this.props.auth.login} className="nav-link">
           <a href="/public">View Profiles</a>
         </div>
+
         {this.props.auth.isAuthenticated() ? 
-          <Fragment>
-              <div className="nav-link">
-                <Link to="/dashboard">Dashboard</Link>
-              </div>
-              <div onClick={() => this.props.auth.logout({...this.props})} className="nav-link">
-                <a href="/">Logout</a>
-              </div>
-          </Fragment>
-        : <div onClick={this.props.auth.login} className="nav-link">
-          <a href="/">Sign Up/Sign In</a>
-        </div>}
+            <Fragment>
+                <div className="nav-link">
+                  <Link to="/dashboard">Dashboard</Link>
+                </div>
+                <div onClick={() => this.props.auth.logout({...this.props})} className="nav-link">
+                  <a href="/">Logout</a>
+                </div>
+            </Fragment>
+        : <div onClick={this.props.auth.login} className="nav-link">Sign Up/Sign In</div>}
       </div>
     );
   }
