57fecb9d9703c9f66e69d79e415fbe989c50a8bb
your <jurgenkela@gmail.com>
2019-01-22 18:24:45 -0500

    set initial params along with state and scroll structure

--- frontend/src/components/InfiniteScroll/InfiniteScroll.js
+++ frontend/src/components/InfiniteScroll/InfiniteScroll.js
@@ -22,6 +22,45 @@ class InfiniteUsers extends Component {
 
     if (error || isLoading || !hasMore) return;
 
-    if()
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      loadUsers();
+    }
+  }
+  componentDidMount() {
+    this.loadUsers();
+  }
+
+  loadUsers = () => {
+    this.setState({ isLoading: true }, () => {
+      request
+        .get("https://randomuser.me/api/?results=10")
+        .then(results => {
+          const nextUsers = results.body.results.map(user => ({
+            email: user.email,
+            name: Object.values(user.name).join(" "),
+            photo: user.picture.medium,
+            username: user.login.username,
+            uuid: user.login.uuid
+          }));
+
+          this.setState({
+            hasMore: this.state.users.length < 100,
+            isLoading: false,
+            users: [...this.state.users, ...nextUsers]
+          });
+        })
+        .catch(err => {
+          this.setState({
+            error: err.message,
+            isLoading: false
+          });
+        });
+    });
+  };
+  render() {
+    const { error, hasMore, isLoading, users } = this.state;
   }
 }
