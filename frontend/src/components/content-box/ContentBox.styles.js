91ad5440143c5e85f068046f20420a8e83c9f441
JoCatherine Griggs <jocafrin@gmail.com>
2019-01-31 01:43:20 -0500

    finished media queries for existing content

--- frontend/src/components/content-box/ContentBox.styles.js
+++ frontend/src/components/content-box/ContentBox.styles.js
@@ -17,11 +17,14 @@ export const Circle = styled.div`
   @media all and (max-width: 839px) {
     left:0;
 
+
   }
   @media all and (max-width: 480px) {
     border-radius:0;
-    height:300px;
-    flex-wrap: wrap;
+    height:350px;
+    width:500px;
+    padding: 1em;
+
    }
 `;
 export const ContentBoxSection = styled.div`
@@ -29,28 +32,29 @@ export const ContentBoxSection = styled.div`
   max-width: 1400px;
 
   width: 100%;
-  text-align: left;
+
   margin: auto;
 
   padding: 2em;
 
   a {
     text-decoration: none;
-    display: flex;
-    justify-content: center;
-    align-items: center;
   }
   @media all and (max-width: 839px) {
    display:flex;
    justify-content: center;
 
   }
+  @media all and (max-width: 480px) {
+    padding:0;
+    }
 `;
 
 export const CTAContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: space-around;
+  color:white;
   h1 {
     font-size: 80px;
     line-height: 100px;
@@ -58,19 +62,40 @@ export const CTAContainer = styled.div`
     color: var(--accent-color);
   }
 
+
   span {
     color: white;
   }
+
+  @media all and (max-width: 480px) {
+   h1 {
+     font-size: 64px;
+   }
+
+   }
+
 `;
 
 export const CallToAction = styled.div`
   display: flex;
   flex-direction: row;
+
+
 `;
 
 export const BtnRow = styled.div`
   display: flex;
   justify-content: space-around;
+  @media all and (max-width: 480px) {
+    display:flex;
+    height: 150px;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+
+
+   }
+
 `;
 
 export const Btn = styled.div`
@@ -101,5 +126,10 @@ export const Btn = styled.div`
     transform: scale(1);
     background-position: 500px;
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+
   }
+  @media all and (max-width: 480px) {
+   width: 350px;
+
+   }
 `;
