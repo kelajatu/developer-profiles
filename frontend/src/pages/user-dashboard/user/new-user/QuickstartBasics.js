e9d58d1c8702e56e68cacfc5630aae748fde7c4d
Alberto-Funk <laptopreneur@gmail.com>
2019-02-05 15:45:10 -0800

    responsive design for intro page, quickstart, additional styling

--- frontend/src/pages/user-dashboard/user/new-user/QuickstartBasics.js
+++ frontend/src/pages/user-dashboard/user/new-user/QuickstartBasics.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react'
 import styled from 'styled-components';
 import axios from 'axios';
+import { centerFlex } from '../../../../global-styles/Mixins';
 
 import { TextInput, Select, TextArea } from 'grommet';
-import {
-  FormSection,
-  LabelContainer,
-  ImageContainer,
-  ButtonContainer
-} from '../../styles/FormStyles';
 
 var noLeaks;
 class QuickstartBasics extends Component {
@@ -172,12 +167,11 @@ class QuickstartBasics extends Component {
     } = this.props.userInfo;
 
     return (
-      <div style={{paddingLeft: '50px'}} >
+      <MainContainer>
         <header>
           <h1 style={{
             fontSize: '3.2rem',
             color: 'rgb(42,42,42)',
-            marginTop: '75px',
             marginBottom: '50px',
             textAlign: 'left',
         }}>Let's Start with the Basics</h1>
@@ -371,9 +365,180 @@ class QuickstartBasics extends Component {
           </div>
         </ButtonContainer>
 
-      </div>
+      </MainContainer>
     )
   }
 }
+
+const MainContainer = styled.div`
+  padding-left: 50px;
+  @media (max-width: 650px) {
+    padding-left: 20px;
+  }
+`;
+
+const FormSection = styled.section`
+  .text-input-container,
+  .select-input-container {
+    margin-bottom: 30px;
+  }
+
+  .text-input-container {
+    /****SKILLS BUTTONS****/
+    .skills-btn {
+      width: 100px;
+      color: black;
+      padding: 8px;
+      font-size: 1.4rem;
+      letter-spacing: 1.5px;
+      background: white;
+      border: solid 1px black;
+      border-radius: 20px;
+      margin-top: 10px;
+      &:hover {
+        cursor: pointer;
+        background: black;
+        color: white;
+      }
+    }
+  }
+  .skillbank {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .skill {
+    cursor: pointer;
+    margin: 5px 5px 5px 0;
+    padding: 0 5px;
+    border: solid grey 1px;
+    border-radius: 5px;
+  }
+  .text-input,
+  #userAreaOfWork,
+  #usercurrentLocation,
+  #userPlacesInterested {
+    width: 85%;
+    @media (max-width: 850px) {
+      width: 95%;
+    }
+  }
+`;
+
+
+const LabelContainer = styled.div`
+  display: flex;
+  align-items: baseline;
+  .success {
+    color: var(--accent-color);
+  }
+  label {
+    color: rgba(42,42,42,.8);
+    font-size: 1.7rem;
+    margin-bottom: 8px;
+    font-weight: bold;
+    line-height: 23px;
+    letter-spacing: 1px;
+    margin-right: 5px;
+  }
+`;
+
+const ImageContainer = styled.div`
+  margin-bottom: 30px;
+  .img-input-sub-container {
+    width: 85%;
+    border: solid 1px rgba(0,0,0,.33);
+    border-radius: 4px;
+    @media (max-width: 850px) {
+      width: 95%;
+    }
+    .img-input-overlay {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      ${centerFlex()};
+    }
+    input[type=file] {
+      padding: 11px 0;
+      width: 100%;
+      height: 100%;
+      opacity: 0;
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
+  .validate {
+    width: 100%;
+    border: none;
+  }
+`;
+
+
+const ButtonContainer = styled.div`
+  width: 80%;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 10px;
+  margin-bottom: 50px;
+
+  .success {
+    color: var(--lp_btn_color);
+  }
+
+  /****ALL OTHER BUTTONS****/
+  button {
+    width: 330px;
+    height: 80px;
+    color: white;
+    padding: 20px 30px;
+    font-size: 2rem;
+    letter-spacing: 1.5px;
+    background-color: var(--accent-color);
+    border: none;
+    border-radius: 100px;
+    ${centerFlex()};
+    margin-left: 25px;
+    margin-right: 25px;
+    &:hover {
+      color: var(--lp_btn_color);
+      transform: scale(1.1);
+      box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+      cursor: pointer;
+    }
+    &:active {
+      transform: scale(1);
+      box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    }
+  }
+
+  a {
+    width: 230px;
+    height: 55px;
+    display: block;
+    margin: auto;
+    text-decoration: none;
+    color: white;
+    padding: 20px 30px;
+    font-size: 2rem;
+    letter-spacing: 1.5px;
+    background-color: var(--lp_btn_color);
+    border: none;
+    border-radius: 100px;
+    ${centerFlex()};
+    &:hover {
+      color:var(--accent-color);
+      transform: scale(1.1);
+      box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+      cursor: pointer;
+    }
+    &:active {
+      transform: scale(1);
+      box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+
+    }
+  }
+`;
 
 export default QuickstartBasics;
