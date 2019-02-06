239c4d6a2a9851473c9508a48f811a451ed179ff
mkerbleski <mkerbles@gmail.com>
2019-02-06 16:58:04 -0700

    got menu button to conditionaly display

--- frontend/src/components/Filter/filter.js
+++ frontend/src/components/Filter/filter.js
@@ -14,16 +14,34 @@ export default class FilterBox extends Component {
     };
   }
 
+  buttons(){
+    return(
+          <div className="buttons">
+            <button onClick={() => this.props.filter(true)}>Search</button>
+            <button onClick={() => this.props.updatePublicPageState(false)}>Clear </button>
+          </div>
+      )
+  }
+  ham(){
+    return(
+        <div className="menu-button-container">
+          <MenuButton menuOpen={this.state.menuOpen}>
+              <i onClick={()=> this.setState({ menuOpen: !this.state.menuOpen })} 
+                class="fa fa-bars"></i>
+          </MenuButton>
+        </div>
+      )
+  }
+
   render() {
+    console.log(this.state.menuOpen)
     return (
       <Grommet theme={filterTheme}>
+        {window.innerWidth < 839 ? this.ham(): null}
         <FilterBoxDiv menu={this.state.menuOpen}>
-          {/* <h2>
-            Showing: <strong>{this.props.publicPageState.usersReturned}</strong> of {this.props.publicPageState.usersFound} possible profiles
-          </h2> */}
-          <h2>
+          {window.innerWidth < 839 ? null :<h2>
             <strong>{this.props.publicPageState.usersFound}</strong>  possible profiles
-          </h2>
+          </h2>}
           <JobTitles
             updatePublicPageState={this.props.updatePublicPageState}
             toggleCheckMarks={this.props.toggleCheckMarks}
@@ -34,19 +52,17 @@ export default class FilterBox extends Component {
             publicPageState={this.props.publicPageState}
             filter={this.props.filter}
           />
-          <Relocate
-            updatePublicPageState={this.props.updatePublicPageState}
-            publicPageState={this.props.publicPageState}
-            filter={this.props.filter}
-          />
-          <div className="buttons">
-            <button onClick={() => this.props.filter(true)}>Search</button>
-            <button onClick={() => this.props.updatePublicPageState(false)}>Clear </button>
+          <div className="relocate-container">
+            <Relocate
+              updatePublicPageState={this.props.updatePublicPageState}
+              publicPageState={this.props.publicPageState}
+              filter={this.props.filter}
+            />
+            {window.innerWidth < 839 ? this.buttons() : null}
           </div>
+          {window.innerWidth < 839 ? null : this.buttons()}
+          {this.state.menuOpen ? this.ham(): null}
         </FilterBoxDiv>
-        <MenuButton menuOpen={this.state.menuOpen}>
-        <i onClick={()=> this.setState({ menuOpen: !this.state.menuOpen })} class="fa fa-bars"></i>
-        </MenuButton>
       </Grommet>
     );
   }
@@ -72,9 +88,9 @@ const filterTheme = {
 
 const MenuButton = styled.div `
   height: 20px;
-  width: 120px;
-  left: calc(50% - 20px);
+  width: 100%;
   top: ${props => props.menuOpen ? 280 : 50}px;
+  justify-content: ${props => props.menuOpen ? 'center' : null};
   margin: auto;
   position: fixed;
   z-index: 1;
@@ -82,8 +98,14 @@ const MenuButton = styled.div `
   font-size: 20px;
   color: grey;
   @media (max-width: 839px) {
-   
-    }
+    /* border: 1px solid red; */
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    margin-bottom: 10px;
+    width: 100%;
+
+  }
 `
 
 const FilterBoxDiv = styled.aside`
@@ -91,7 +113,7 @@ const FilterBoxDiv = styled.aside`
   background-color: white;
   z-index: 10;
   width: 272px;
-  height: 100%;
+  height: auto;
   padding-top: 80px;
   border-right: solid 0.5px #dbdee2;
   display: flex;
@@ -99,6 +121,54 @@ const FilterBoxDiv = styled.aside`
   flex-direction: column;
   /* border: 1px solid blue; */
   overflow: auto;
+
+  .relocate-container {
+    @media (max-width: 839px) {
+      /* border: 1px solid red; */
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+    .buttons {
+      /* border: 1px solid red; */
+      width: 90%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      @media (max-width: 839px) {
+        /* border: 1px solid blue; */
+        width: 100%;
+        justify-content: center;   
+      }
+      button {
+        width: 70%;
+        color: black;
+        margin: 10px;
+        padding: 5px;
+        font-size: 20px;
+        letter-spacing: 1.5px;
+        background: white;
+        border: solid 1px black;
+        border-radius: 20px;
+        /* display: flex;
+        align-items: center;
+        justify-content: center; */
+        outline-style: none;
+        &:hover {
+          cursor: pointer;
+          background: black;
+          color: white;
+        }
+        /* @media (max-width: 839px) {
+        width: 20%;
+        height: 50px;
+        } */
+        @media (max-width: 839px) {
+          width: 100%;
+        }
+      }
+    }
+  }
   h1 {
     font-size: 20px;
     margin-bottom: 5px;
@@ -107,39 +177,8 @@ const FilterBoxDiv = styled.aside`
     font-size: 18px;
     margin-bottom: 5px;
   }
-  .buttons{
-    /* border: 1px solid red; */
-    width: 90%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    button {
-      width: 70%;
-      color: black;
-      margin: 10px;
-      padding: 5px;
-      font-size: 20px;
-      letter-spacing: 1.5px;
-      background: white;
-      border: solid 1px black;
-      border-radius: 20px;
-      /* display: flex;
-      align-items: center;
-      justify-content: center; */
-      outline-style: none;
-      &:hover {
-        cursor: pointer;
-        background: black;
-        color: white;
-      }
-      @media (max-width: 839px) {
-      width: 20%;
-      height: 50px;
-      }
-    }
-  }
   @media (max-width: 839px) {
-    border: none;
+    /* border: 1px solid red; */
     width: 100%;
     padding-right: 20px;
     height: ${props => props.menu ? 300 : 0 }px;
@@ -150,17 +189,15 @@ const FilterBoxDiv = styled.aside`
     h1 {
       font-size: 15px;
       margin-bottom: 0;
-  }
+    }
     h2 {
       font-size: 15px;
       margin-bottom: 0;
-  }
-    @media (max-width: 480px) {
-    height: ${props => props.menu ? 100 : 0 }vh;
     }
-  }
-
- 
+    @media (max-width: 480px) {
+      height: ${props => props.menu ? 100 : 0 }vh;
+    }
+  } 
 `;
 
 
