dd6c510b7334af12ca8c3e0c50bca47407e5c9db
your <jurgenkela@gmail.com>
2019-02-05 20:17:58 -0500

    footer container created and styled

---
+++ frontend/src/components/Footer/Footer.styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+  background-color: #292c2f;
+  box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.12);
+  box-sizing: border-box;
+  width: 100%;
+  text-align: left;
+  font: normal 16px sans-serif;
+  padding: 20px;
+  position: fixed;
+  margin-top: auto;
+  bottom: 0;
+`;
