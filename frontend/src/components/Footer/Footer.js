ecd40fe184c2761212d3a4a7642d2a1cc8e37b15
your <jurgenkela@gmail.com>
2019-02-06 12:24:46 -0500

    created content left

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -2,7 +2,24 @@ import React from "react";
 import { FooterContainer } from "./Footer.styles";
 
 const Footer = () => {
-  return <FooterContainer />;
+  return (
+    <FooterContainer>
+      <div className="footer-right">
+        <a href="#">
+          <i className="fa fa-facebook" />
+        </a>
+        <a href="#">
+          <i className="fa fa-twitter" />
+        </a>
+        <a href="#">
+          <i className="fa fa-linkedin" />
+        </a>
+        <a href="#">
+          <i className="fa fa-github" />
+        </a>
+      </div>
+    </FooterContainer>
+  );
 };
 
 export default Footer;
